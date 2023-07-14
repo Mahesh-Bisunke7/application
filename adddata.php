334b263e6db997481d76e8688645bedfe5e6abbc
Mahesh-Bisunke <maheshbisunke@triton.edu.np>
2023-07-15 01:34:26 +0545

    adddata.php is changed

--- adddata.php
+++ adddata.php
@@ -1,7 +1,19 @@
 <?php 
 $name=$_POST['name'];
-$email=$_POST['email'];
 $phone=$_POST['phone'];
 $address=$_POST['address'];
+
+
+echo $name, $phone ,$address;
+include 'db.php';
+
+$sql= "INSERT INTO userinfo(name,phone,address)VALUES('$name','$phone','$address')";
+
+$result=mysqli_query($conn,$sql);
+
+if($result){
+    header('location:index.php');
+}
+
 
 ?>
