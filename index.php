aa7bfe5ccbbe9fcd7aa4782ca208a8544c596984
Mahesh-Bisunke <maheshbisunke@triton.edu.np>
2023-07-14 18:42:48 +0545

    index.html created

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Application</title>
+</head>
+<body>
+    <fieldset>
+        <h2>Contact Application</h2>
+        <div>
+            <form action="" method="post">
+
+            </form>
+        </div>
+    </fieldset>
+</body>
+</html>
