72e0fb80213687d86c5a3436714a7818086ee5e9
Mahesh-Bisunke <maheshbisunke@triton.edu.np>
2023-07-15 02:10:29 +0545

    edit.php again modified

--- edit.php
+++ edit.php
@@ -2,35 +2,32 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Application</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Contact APP</title>
 </head>
 <body>
-    <fieldset class="container">
-        <h2>Update Contact</h2>
-        <?php 
-        include 'db.php';
-        $id = $_GET['id'];
-        $sql="SELECT * FROM userinfo WHERE id=".$id;
-        $result=mysqli_query($conn, $sql);
-        if($result){
-            $row=mysqli_fetch_assoc($result);
-            $contactname=$row['name'];
-            $contactphone=$row['phone'];
-            $contactaddress=$row['address'];
-}
-         ?>
-        <div>
-            <form action="update.php" method="post">
-                <input type="text" name="name" id="username" value="<?php global $contactname; echo $contactname ?>"required> <br> <br>
-                <input type="number" name="phone" id="phone" value="<?php global $contactphone; echo $contactphone ?>"required> <br> <br>
-                <input type="text" name="address" id="address" value="<?php global $contactaddress; echo $contactaddress ?>" required> 
-                <input type="hidden" name="id" id="id" value="<?php global $id; echo $id ?>"  required> 
-                <input type="submit" value="Update">
-            </form>
+   <h2>Update Contact</h2>
+    <?php
+    include 'db.php';
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM names WHERE id=".$id;
+    $result = mysqli_query($conn, $sql);
+    if($result){
+        $row = mysqli_fetch_assoc($result);
+        $contactname = $row['name'];
+        $contactphone = $row['phone'];
+    }
+    ?>
+    <form action="update.php" method="post">
+        <div class="main">
+            <label for="name">Name:</label><br>
+            <input type="text" name="name" required value="<?php global $contactname; echo $contactname?>"id="name"><br><br>
+            <label for="contact">Number:</label><br>
+            <input type="number" name="contact" required value="<?php global $contactphone; echo $contactphone?>" id="contact">
+            <input type="hidden" name="id" id="id" value="<?php global $id; echo $id?>" required><br>
+            <input type="submit" value="Update">
         </div>
-    </fieldset>
-    <hr>
-   </body>
+    </form>
+</body>
 </html>
